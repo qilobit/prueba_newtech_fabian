4bdde1abd0a0c784d03e8267e9925a9110e4925c
Qilobit <Qilobit@gmail.com>
2020-07-13 17:59:52 -0400

    Fix date showing format

--- client/src/app/utils/date.ts
+++ client/src/app/utils/date.ts
@@ -1,9 +1,16 @@
 import * as moment from 'moment';
 
 export const formatDate = (date) => {
-  return moment(date).format('YYYY-MM-DD');
+  return moment(cleanDate(date)).format('DD-MM-YYYY');
 }
 
 export const editingDate = (date) => {
-  return moment(date).format('YYYY-MM-DD');
+  return moment(cleanDate(date)).format('YYYY-MM-DD');
+}
+
+function cleanDate(date) {
+  if (date) {
+    return String(date).split('T')[0];
+  }
+  return date;
 }
